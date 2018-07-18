ff554ee182dcb377dc7cad0650ad5da08dbf70a4
Hans.W <hanswang2012@gmail.com>
2018-07-18 19:42:33 +1000

    copy php72 config over

--- php72/my_init.d/02-setup-xdebug.sh
+++ php72/my_init.d/02-setup-xdebug.sh
@@ -7,9 +7,9 @@ echo "================================"
 # Only enable when env variable defined as "true"
 if [ $PHP_XDEBUG_ENABLED = "false" ] || [ $PHP_XDEBUG_ENABLED = "False" ] ; then
     echo "==> Disabling"
-    phpdismod -s fpm -v 7.0 xdebug
+    phpdismod -s fpm -v 7.2 xdebug
 else
     echo "==> Enabling"
-    phpenmod -s fpm -v 7.0 xdebug
+    phpenmod -s fpm -v 7.2 xdebug
 fi;
 
--- php72/my_init.d/04-setup-environment.sh
+++ php72/my_init.d/04-setup-environment.sh
@@ -7,17 +7,17 @@ echo " "
 
 if [ $PHP_ENV = "production" ] || [ $PHP_ENV = "Production" ] ; then
     echo "==> Enabling production"
-    phpdismod -s ALL -v 7.0 development
-    phpenmod -s ALL -v 7.0 production
+    phpdismod -s ALL -v 7.2 development
+    phpenmod -s ALL -v 7.2 production
 else
     echo "==> Enabling development"
-    phpdismod -s ALL -v 7.0 production
-    phpenmod -s ALL -v 7.0 development
+    phpdismod -s ALL -v 7.2 production
+    phpenmod -s ALL -v 7.2 development
 fi;
 
 echo "================================"
 echo "==> Reloading PHP7-FPM"
 echo "================================"
-service php7.0-fpm reload
+service php7.2-fpm reload
 
 echo "================================"
